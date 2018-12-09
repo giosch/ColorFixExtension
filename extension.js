1d996e7de1cad3f2ebda2925049e5cc77cdd9cab
giosch <giovannischiavon96@gmail.com>
2018-12-09 22:13:30 +0100

    added toggle

--- extension.js
+++ extension.js
@@ -6,6 +6,9 @@ const Shell   = imports.gi.Shell;
 
 const HOTKEY = "<Super>i::"
 
+var connectionID;
+var active = false;
+
 const ColorFixEffect = new Lang.Class({
 	Name: 'ColorFixEffect',
 	Extends: Clutter.ShaderEffect,
@@ -25,13 +28,43 @@ var shaderSource;
 function init() {
 	shaderSource = Shell.get_file_contents_utf8_sync("/home/giosch/.shader.glsl");
 }
-function disable() {
+
+function setupHotKey(){
+	Main.keybindingManager.addHotKey('colorfix-on', 'space', Lang.bind(this, this.toggle));
+}
+function cleanupHotKey(){
+	Main.keybindingManager.removeHotKey('colorfix-on', 'space', Lang.bind(this, this.toggle));
+}
+
+function enable(){
+	setupHotKey();
+	toggle();
+}
+
+function disable(){
+	cleanupHotKey();
+	deactivate();
+}
+
+function toggle(){
+		if (active) {
+			active = false;
+			deactivate();
+		} else {
+			active = true;
+			activate();
+		}
+}
+
+function deactivate() {
+	global.display.disconnect(connectionID);
 	global.get_window_actors().forEach(function(actor) {
 			actor.remove_effect_by_name('colorfix');
 	});
 }
-function enable() {
-		global.display.connect('window-created', Lang.bind(this, this.scanAndApply));
+
+function activate() {
+		connectionID = global.display.connect('window-created', Lang.bind(this, this.scanAndApply));
 		scanAndApply();
 }
 
